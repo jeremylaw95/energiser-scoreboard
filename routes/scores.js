5c7975f8c6c4e9ed4c5156b9c45a77794b862efc
Aftab Hussain <aftabhussainprojects@gmail.com>
2020-10-15 16:19:11 +0100

    API working

--- routes/scores.js
+++ routes/scores.js
@@ -8,24 +8,29 @@ router.get("/", async function (req, res, next) {
   res.json({ success: true, data: scores });
 });
 
-// router.get("/:id", function (req, res) {
-//   const id = req.params.id;
-//   console.log(req.params.id);
-//   const joke = getJokeById(id);
-//   res.json({ success: true, payload: joke });
-// });
+router.get("/:id", async function (req, res) {
+  const {id} = req.params;
+  const player = await getPlayerById(id);
+  res.json({ success: true, payload: player });
+});
+
+router.post("/name", async function (req, res) {
+    const {name} = req.body;
+    //console.log(req.body)
+    const player = await getPlayerByName(name);
+    res.json({ success: true, payload: player });
+});
 
 router.post("/", async function (req, res) {
-  const { name, score } = req.body;
+  const {name, score} = req.body;
   const id = await addPlayer(name, score);
   res.json({ success: true, message: `${name} has been added with id ${id}` });
 });
 
-router.put("/:id/:score", async function (req, res) {
-  const { id, score } = req.params;
+router.put("/", async function (req, res) {
+  const { id, score } = req.body;
   const result = await updatePlayerScoreByID(id, score);
-  res.json({ success: true, message: result });
-  // console.log(req.params)
+  res.json({ success: true, message: `Player with id:${id} now has a score of ${score}` });
 });
 
 module.exports = router;
