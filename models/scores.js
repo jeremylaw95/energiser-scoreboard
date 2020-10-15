ff97264c65048a9a0e4a4fe4fa322e0d6af08d43
Jeremy Law <jeremylaw2308@gmail.com>
2020-10-15 14:13:56 +0100

    adding functions

--- models/scores.js
+++ models/scores.js
@@ -3,7 +3,7 @@ const { query } = require("../db/index");
 // Model Functions
 // 1$      Add Player (with a score optional, 0 if no score given)
 // 4&      Get all table - returns all the scores
-// 2(      Get an individual players score
+// 2(      Get an individual players score by name/score
 // 7*      Get top 5
 // 3.3^    Update Player Name
 // 5%      Update Score (select a player and add 
@@ -14,4 +14,18 @@ async function addPlayer(name, score = 0) {
     return result.rows[0].id;
 }
 
-addPlayer("Aftab", 5);
+async function getScores() {
+    const result = await query("SELECT * FROM scores");
+    console.log(result);
+    return result;
+}
+
+async function getPlayerScoreById(id) {
+    const result = await query("SELECT * FROM scores WHERE id = $1;", [id]);
+    console.log(result);
+    return result;
+}
+
+
+
+getPlayerScoreById(2);
